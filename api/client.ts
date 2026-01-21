c26048fc81d33ac9d47ca58481933372db72d745
rishugren03 <rishugren03@gmail.com>
2026-01-21 05:48:02 +0530

    fix: environment error from vercel, add backend url

--- api/client.ts
+++ api/client.ts
@@ -1,19 +1,8 @@
 import axios from 'axios';
-import { Platform } from 'react-native';
 import { LeaderboardEntry, SearchResult } from '../types';
 
-import Constants from 'expo-constants';
-
 const getBaseUrl = () => {
-  if (Platform.OS === 'web') return 'http://localhost:8080/api';
-  
-  const hostUri = Constants.expoConfig?.hostUri;
-  if (hostUri) {
-    const ip = hostUri.split(':')[0];
-    return `http://${ip}:8080/api`;
-  }
-  
-  return 'http://10.0.2.2:8080/api';
+  return process.env.EXPO_BASE_URL || "https://leaderboard-backend-845642926246.asia-south1.run.app/api";
 };
 
 export const api = axios.create({
